3a9236684cce7315ee41d4692ae126d7f9f8c2b7
AastikGautam <34084056+AastikGautam@users.noreply.github.com>
2018-02-10 16:03:57 +0530

    Add files via upload

---
+++ Date1.java
@@ -0,0 +1,27 @@
+
+	import java.text.SimpleDateFormat;
+
+	import java.util.Date;
+
+
+
+	public class Date1 {
+
+		
+
+		   public static void main(String args[]) {
+
+			      Date todayDate = new Date( );
+
+			      SimpleDateFormat dateFormat = 
+
+			      new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+
+
+
+			      System.out.println("Current Date: " + dateFormat.format(todayDate));
+
+			   }
+
+		}
+
